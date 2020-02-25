d524b009852eb79983da03f23079949a4d320a16
JP Davey <JayaprakashDavey@users.noreply.github.com>
2020-02-25 12:34:59 +0530

    Create runit.bat

---
+++ artifacts/Shipping/runit.bat
@@ -0,0 +1,1 @@
+java -cp shipping-services-8.6.0.RELEASE.jar -Dskava.redis.maxHeap=2048M -Dcamel.logging.level.root=info -Dspring.profiles.active=dev -Dspring.cloud.zookeeper.enabled=false -Dspring.cloud.zookeeper.config.watcher.enabled=false -Dloader.path=target\final org.springframework.boot.loader.PropertiesLauncher -debug
